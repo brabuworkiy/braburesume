20596ef8829080a9c868762e616581eea1a1ca1f
NarayanaBrabuN <swastilocalhost.com>
2024-05-03 00:18:06 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,261 @@
+import React from 'react';
+import './App.css';
+import Gmail from "../src/images/gmail.png";
+import Call from "../src/images/calls.png";
+import Home from "../src/images/home.png";
+import MongoDB from "../src/images/mongodb.png";
+import Express from "../src/images/express.png";
+import React1 from "../src/images/React.png";
+import Node from "../src/images/node.png";
+import Html from "../src/images/html.png";
+import Css from "../src/images/css.png";
+import JavaScript from "../src/images/js.png";
+import Git from "../src/images/git.png";
+import Python from "../src/images/python.png";
+import AUTOMATIONTESTING from "../src/images/tosca.png";
+import FLUTTER from "../src/images/flutter.png";
+import Microsoft from "../src/images/ms.png";
+import Mysql from "../src/images/mysql.png";
+import AZ from "../src/images/az.png";
+import Putty from "../src/images/putty.png";
+import Filezilla from "../src/images/filezilla.png";
+import Jira from "../src/images/jira.png";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Narayana Brabu .N</h1>
+    
+        <p className="job-title">Full - Stack Developer (MERN)</p>
+      </header>
+
+      <div className='cr'>
+        <h2 className='CI'>Career Objective</h2>
+       
+        <p className='CareerObjective'>To Enhance My Working Capabilities, Professional - Skills to Serve My Organization in The Best Possible Way.</p>
+
+      </div>
+
+      <section className="main-content">
+        <div className="left-column">
+          <h2 className='CI'>Contact Information</h2>
+          <br />
+          <ul className="contact-info">
+            <li>
+              <img src={Gmail} alt="Email" />
+              <a href="mailto:brabudeveloper14@gmail.com"><span className='mail'>brabudeveloper14@gmail.com</span></a>
+            </li>
+            <li>
+              <img src={Call} alt="Call" />
+              <a href="tel:7418671314"><span className='call'>7418671314</span></a>
+            </li>
+            <li>
+              <img src={Home} alt="Email" />DIndigul,TamilNadu - 624403
+            </li>
+          </ul>
+          <br />
+          <h2 className='SK'>Programming Skills</h2>
+          <br />
+          <ul className="skills">
+            <li>MERN STACK</li>
+            <li>
+              <img src={Mysql} alt="Email" />MYSQL
+            </li>
+            <li>
+              <img src={MongoDB} alt="Email" />MONGODB
+            </li>
+            <li>
+              <img src={Express} alt="Email" />EXPRESS.JS
+            </li>
+            <li>
+              <img src={React1} alt="Email" />REACT.JS
+            </li>
+            <li>
+              <img src={Node} alt="Email" />NODE.JS
+            </li>
+            <li>
+              <img src={Html} alt="Email" />HTML 5
+            </li>
+            <li>
+              <img src={Css} alt="Email" />CSS 3
+            </li>
+            <li>
+              <img src={JavaScript} alt="Email" />JAVASCRIPT
+            </li>
+            <li>
+              <img src={Git} alt="Email" />Git, GitHub
+            </li>
+            <li>
+              <img src={Python} alt="Email" />PYTHON
+            </li>
+            <li>
+              <img src={AUTOMATIONTESTING} alt="Email" />AUTOMATION TESTING(Tricentis Tosca)
+            </li>
+            <li>
+              <img src={FLUTTER} alt="Email" />Flutter Basics
+            </li>
+            <li>
+              <img src={Microsoft} alt="Email" />Microsoft AZURE Fundamentals
+            </li>
+          </ul>
+          <br />
+          <h2 className='CI'>Certification</h2>
+          <br />
+          <ul className="skills">
+            <li>
+              <img src={AZ} alt="Email" />Microsoft AZ-900
+            </li>
+          </ul>
+          <br />
+          <h2 className='CI'>Tools Used</h2>
+          <br />
+          <ul className="skills">
+            <li>
+              <img src={Putty} alt="Email" />Putty
+            </li>
+            <li>
+              <img src={Filezilla} alt="Email" />FileZilla
+            </li>
+            <li>
+              <img src={Jira} alt="Email" />Jira Board
+            </li>
+          </ul>
+
+        </div>
+        <div className="right-column">
+          <h2 className='ED'>Education</h2>
+          <ul className="education">
+            <br />
+            <li>
+              <strong>Bachelor of Engineering</strong>
+              <p>N.P.R College of Engineering & Technology (Anna University ) | 2015 - 2018</p>
+            </li>
+          </ul>
+    
+          <h2 className='EX'>Experience <span id='ex'>(Over 2.1 Years)</span> </h2>
+          <ul className="experience">
+            <br />
+          
+     
+            <h4>WORKIY Technologies PVT LTD | Dec 2022 - (To - till date)</h4>
+            <br />
+            <strong> Project Details :</strong>
+   
+            <table id='t1'>
+  <thead>
+    <tr>
+      <th id='tc1'>Organization</th>
+      <th> WORKIY - TECHNOLOGIES</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td id='tc1'>Client</td>
+      <td>Workiy Academy</td>
+    </tr>
+    <tr>
+      <td id='tc1'>Team Size</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <td id='tc1'>Role</td>
+      <td>Full Stack Developer</td>
+    </tr>
+    <tr>
+      <td id='tc1'>Tools Used</td>
+      <td>GIT, Git-Hub, VS-code, JIRA Software, Filezilla, Putty.</td>
+    </tr>
+
+  </tbody>
+</table>
+<ul className='pd'>
+            <li>
+              
+
+              <strong>1. Project Description :</strong>
+              <li>Workiy Academy serves as an innovative E-Learning platform catering to IT professionals and students, providing them with an opportunity to enhance their diverse IT knowledge. Interested candidates can easily navigate the platform to search for a plethora of courses tailored to meet their specific needs. The seamless interface allows users to register effortlessly for both online and offline classes, ensuring flexibility and accessibility in their pursuit of up skilling within the realm of Information Technology.</li>
+            </li>
+            <li>
+              <strong>Roles & Responsibilities :</strong>
+              <li>Create responsive and user-friendly interfaces using HTML, CSS, and JavaScript frameworks like React.js.Design and implement server-side logic, APIs, and databases using technologies such as Node.js </li>
+              <li>Develop and maintain databases, ensuring efficient data storage and retrieval.</li>
+              <li>Integrate front-end and back-end components to ensure seamless communication between the user interface and server-side applications.
+                Work closely with UI/UX designers to implement visually appealing and user-centric designs.
+              </li>
+              <li>Utilize version control systems such as Git to manage and track changes in the code base.
+                Diagnose and troubleshoot issues reported by users or identified during testing.
+              </li>
+            </li>
+            </ul>
+            <strong> Project Details :</strong>
+                  
+            <table id='t2'>
+  <thead>
+    <tr>
+      <th id='tc1'>Organization</th>
+      <th>WORKIY - TECHNOLOGIES</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td id='tc1'>Client</td>
+      <td>JEVITTY - (Canada)</td>
+    </tr>
+    <tr>
+      <td id='tc1'>Team Size</td>
+      <td>7</td>
+    </tr>
+    <tr>
+      <td id='tc1'>Role</td>
+      <td>Full Stack Developer & Tosca Automation Tester</td>
+    </tr>
+   
+
+  </tbody>
+</table>
+
+<ul className='pd'>
+            <li>
+
+              
+            <strong>2. Project Description :</strong>
+               <li>Jevitty is a new way of looking at how we live in order to create an optimal balance of body and mind. Together we can transform what human longevity means, and help redefine your true potential. Through community, education and analysis, Jevitty is dedicated to unlocking the key to quality longevity for its users.</li>
+               <strong>Roles & Responsibilities :</strong>
+               <li>Developed, executed the automation scripts and analyzed the result using Tosca.
+Very Good Experience on Tosca Commander.
+	Good work exposure on Exception Handling in Tosca.
+</li>
+               <li>Collaborated with the testing team to ensure the delivery of a reliable and bug-free product.
+	Experienced in working on Web Hosting Management (WHM).
+Uploading Local Developed files into Server using Filezilla.
+Interacted with client on regular basis to clarify the questions.
+</li>
+</li>
+              
+              <h4>CADDCAE Technologies | March 2022 - Nov 2021 (MERN Stack Developer & Trainer)</h4>
+       
+              <li> <strong> Work Description :</strong>  <li>Managed and administered web hosting servers, ensuring smooth operations and resolving technical issues.</li>
+              <li>And also, I have been working as a MERN stack trainer at CADDCAE Technologies.</li></li>
+           
+           
+          </ul>
+          </ul>
+          <br />
+          <h2 className='DE'>Declaration</h2>
+          <ul className="declaration">
+            <br />
+            <li>
+              <p>I genuinely declare that all the facts mentioned above are true to my faith, and I am answerable for its accuracy.</p>
+            </li>
+          </ul>
+        </div>
+      </section>
+      <footer className="footer">
+        <p>Thank You</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
